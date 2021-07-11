21d9f5fd0f65642c477a7274333094edfafbc5af
Simon Drake <simondrake1990@gmail.com>
2021-07-11 21:05:08 +0100

    Create and view Categories from the admin page

--- src/stores/AdminStore/AdminStore.js
+++ src/stores/AdminStore/AdminStore.js
@@ -12,6 +12,10 @@ class AdminStore {
 
   adminState = {
     users: [],
+    categories: [],
+
+    userError: null,
+    categoryError: null,
   };
 
   createUser = ({
@@ -27,6 +31,8 @@ class AdminStore {
       console.log("No token provided");
       return;
     }
+
+    this.adminState.userError = null;
 
     const url = config.server.baseURL + "/users";
 
@@ -45,7 +51,9 @@ class AdminStore {
         });
       })
       .catch((err) => {
-        console.log(err?.response?.data);
+        runInAction(() => {
+          this.adminState.userError = err?.response?.data?.message;
+        });
       });
   };
 
@@ -72,6 +80,63 @@ class AdminStore {
       })
       .catch((err) => {
         console.log(err?.response?.data);
+      });
+  };
+
+  listCategories = (token) => {
+    if (!token) {
+      console.log("No token provided");
+      return;
+    }
+
+    const url = config.server.baseURL + "/categories";
+
+    axios({
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      url,
+    })
+      .then((res) => {
+        runInAction(() => {
+          this.adminState.categories = res.data.categories;
+        });
+      })
+      .catch((err) => {
+        console.log(err?.response?.data);
+      });
+  };
+
+  createCategory = ({ token, name, color, description }) => {
+    if (!token) {
+      console.log("No token provided");
+      return;
+    }
+
+    this.adminState.categoryError = null;
+
+    const url = config.server.baseURL + "/categories";
+
+    axios({
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      url,
+      data: { name, color, description },
+    })
+      .then((res) => {
+        runInAction(() => {
+          this.adminState.categories.push(res?.data?.category);
+        });
+      })
+      .catch((err) => {
+        runInAction(() => {
+          this.adminState.categoryError = err?.response?.data?.message;
+        });
       });
   };
 }
